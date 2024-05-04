ca15959bc278d06736013f6d3ae3e1311dcfee47
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-05 05:04:29 +0800

    Refactor sign-up page UI

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -4,6 +4,9 @@ import { SubmitButton } from "@/components/SubmitButton";
 import { signUpAction } from "@/lib/actions";
 import { useState } from "react";
 import Link from "next/link";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 
 export default function SignUpPage() {
   const [error, setError] = useState<string>("");
@@ -24,48 +27,58 @@ export default function SignUpPage() {
   };
 
   return (
-    <main className="flex items-center justify-center flex-col gap-4 pt-[10vh] text-white">
-      <Link
-        href="/"
-        className="bg-[hsl(0,0%,7%)] hover:bg-[hsl(0,0%,10%)] p-2 px-8 rounded-lg"
-      >
-        {"<-"} Home
-      </Link>
-      <h1 className="text-5xl text-center bg-[hsl(0,0%,7%)] p-4 rounded-sm font-semibold">
-        Sign Up Page
-      </h1>
-      <p className="text-red-500">{error}</p>
-      <form action={handleFormSubmit} className="flex flex-col gap-2">
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          className="p-2 px-4 rounded-sm outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
-        />
-
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className="p-2 px-4 rounded-sm outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
-        />
-
-        <input
-          type="password"
-          name="password"
-          placeholder="●●●●●●●"
-          className="p-2 px-4 rounded-sm outline-none drop-shadow-sm bg-[hsl(0,0%,10%)]"
-        />
-        <SubmitButton
-          pendingText="Creating account..."
-          className="p-2 bg-[hsl(0,0%,7%)] rounded-lg"
+    <main className="flex items-center justify-center pt-[10vh] ">
+      <div className="flex items-center justify-center flex-col glass p-8 gap-4">
+        <div className="text-md flex items-center flex-col sm:text-4xl font-bold ">
+          <h1 className="text-sm ">Diwata Pet Overload </h1>
+          <Separator className="bg-black" />
+          <span>Sign Up</span>
+        </div>
+        <p className="text-red-500">{error}</p>
+        <form
+          action={handleFormSubmit}
+          className="flex flex-col gap-2 w-[300px]"
         >
-          Create Account
-        </SubmitButton>
-      </form>
-      <Link href="/sign-in" className="text-gray-400">
-        I already have an account
-      </Link>
+          <Label htmlFor="fullName">Full Name</Label>
+          <Input
+            type="text"
+            id="fullName"
+            name="fullName"
+            placeholder="John Doe"
+            className="p-2 px-4 rounded-md outline-none drop-shadow-sm"
+            required
+          />
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="hello@example.com"
+            className="p-2 px-4 rounded-md outline-none drop-shadow-sm"
+            required
+          />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="●●●●●●●"
+            className="p-2 px-4 rounded-md outline-none drop-shadow-sm"
+          />
+          <SubmitButton
+            pendingText="Creating account..."
+            className="p-2 bg-brownPri hover:bg-brownSec rounded-lg"
+          >
+            Sign In
+          </SubmitButton>
+        </form>
+        <Link
+          href="/sign-in"
+          className="text-amber-900 font-semibold hover:underline pb-5"
+        >
+          I already have an account
+        </Link>
+      </div>
     </main>
   );
 }
