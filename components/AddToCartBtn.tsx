23db7dd54a138e20a05a00c6a3f313520e8d5abd
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-05 19:16:40 +0800

    Add AddToCartBtn component

---
+++ components/AddToCartBtn.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { useState, type ComponentProps } from "react";
+import { Button } from "./ui/button";
+import { useSession } from "next-auth/react";
+import {
+  CheckCheckIcon,
+  CircleCheckIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type Props = ComponentProps<"button"> & {
+  productID: string;
+};
+
+export function AddToCardBtn({ children, productID, ...props }: Props) {
+  const session = useSession();
+  const [pending, setPending] = useState(false);
+  const [added, setAdded] = useState(false);
+
+  const handleAddToCart = async () => {
+    setPending(true);
+    setTimeout(() => {
+      setPending(false);
+      setAdded(true);
+    }, 2000);
+  };
+
+  return (
+    <Button
+      {...props}
+      onClick={handleAddToCart}
+      disabled={pending || added}
+      variant="secondary"
+      className={cn(
+        `w-full font-semibold`,
+        added
+          ? `bg-emerald-600 text-white hover:bg-emerald-500`
+          : `bg-[hsl(240,5%,92%)] hover:bg-[hsl(240,5%,85%)] text-black`
+      )}
+    >
+      {added
+        ? "Added to Cart!"
+        : pending
+        ? "Adding To Cart . .  ."
+        : "Add to Cart"}
+      {added ? (
+        <CircleCheckIcon size={20} className="ml-2" />
+      ) : (
+        <ShoppingCartIcon size={16} className="ml-2" />
+      )}
+    </Button>
+  );
+}
