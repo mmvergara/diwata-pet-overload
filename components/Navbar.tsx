178b08fcb72ea5bfb133d397154fd7e607f64d03
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-05 10:08:42 +0800

    Update Navbar component with Link component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,18 +8,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="py-2 bg-[#6b3614] text-white flex justify-center items-center px-4 drop-shadow-md shadow-lg">
       <div className="w-full max-w-[1240px] h-[40px] flex justify-between items-center">
-        <h1 className="text-2xl font-bold">logo</h1>
+        <Link href="/" className="text-2xl font-bold">logo</Link>
         {session.status !== "loading" && (
           <div className="font-semibold">
-            <a href="#" className="">
-              Home
-            </a>
-            <a href="#" className="ml-4">
-              About
-            </a>
-            <a href="#" className="ml-4">
-              Contact
-            </a>
             {session.status === "unauthenticated" && (
               <Link href="/sign-in">
                 <Button
