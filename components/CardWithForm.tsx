148ce47c332d30fdbb22be4caf454c27ea1bbbbc
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-05 10:08:40 +0800

    Refactor CardWithForm component and add click event handlers

--- components/CardWithForm.tsx
+++ components/CardWithForm.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -17,10 +16,15 @@ import {
 } from "lucide-react";
 import { Badge } from "./ui/badge";
 
-export function CardWithForm() {
+export function ProductCard() {
   return (
-    <Card className="w-[300px] rounded-xl shadow-md">
-      <CardHeader className="p-6 pb-2">
+    <Card className="w-[300px] rounded-xl shadow-md hover:shadow-xl hover:scale-105 transition-all cursor-pointer">
+      <CardHeader
+        className="p-6 pb-2"
+        onClick={() => {
+          console.log("clicked card");
+        }}
+      >
         <Image
           src="https://utfs.io/f/5d3ca213-6a52-4733-9be3-6ce3fb82f921-g0gqe4.jpg"
           width={300}
@@ -29,10 +33,15 @@ export function CardWithForm() {
           className="rounded-md"
         />
       </CardHeader>
-      <CardContent className="p-6 pt-0 pb-2">
+      <CardContent
+        className="p-6 pt-0 pb-2"
+        onClick={() => {
+          console.log("clicked card");
+        }}
+      >
         <div className="flex justify-between">
-          <p className="font-bold">Blue Collar</p>
-          <Badge className="bg-[#6b3614]">₱ 300.00</Badge>
+          <p className="font-bold hover:underline">Blue Collar</p>
+          <Badge className="bg-brownPri hover:bg-brownSec">₱ 300.00</Badge>
         </div>
         <div className="flex">
           <StarIcon size={16} className="text-amber-300" fill="#fbbf24" />
@@ -46,6 +55,9 @@ export function CardWithForm() {
         <Button
           variant="secondary"
           className="w-full bg-[hsl(240,5%,92%)] hover:bg-[hsl(240,5%,85%)] font-semibold"
+          onClick={() => {
+            console.log("clicked add to cart");
+          }}
         >
           Add to Cart <ShoppingCartIcon size={16} className="ml-2" />
         </Button>
