652cec10b44e4829d7cbd5d1e2f5618b444f4fee
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-06 18:11:46 +0800

    Add checkout functionality to cart page

--- app/u/cart/page.tsx
+++ app/u/cart/page.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { ShoppingBag } from "lucide-react";
 
 const CartPage = () => {
   return (
     <main className="flex flex-wrap items-center justify-center gap-4 pt-[10vh] text-white">
-      <Card className="mx-4 flex w-full max-w-[1000px] flex-col items-center justify-center gap-4 p-4 md:flex-row md:items-start">
-        
+      <Card className="mx-4 flex w-full max-w-[500px]  flex-row flex-wrap items-center justify-around gap-4 p-4 ">
+        <div className="grow rounded-md bg-slate-100 p-2 font-semibold">
+          Total Items: 14
+        </div>
+        <div className="grow rounded-md bg-slate-100 p-2 font-semibold">
+          Checkout Price: 200,000.00
+        </div>
+        <Button className="grow">
+          Checkout
+          <ShoppingBag size={16} className="ml-2" />
+        </Button>
       </Card>
     </main>
   );
