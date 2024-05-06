bc4f5603697778436d9d340c793db70f70493a04
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-06 17:43:32 +0800

    Add Card component to CartPage

--- app/u/cart/page.tsx
+++ app/u/cart/page.tsx
@@ -1,5 +1,13 @@
+import { Card } from "@/components/ui/card";
+
 const CartPage = () => {
-  return <>Cart Page</>;
+  return (
+    <main className="flex flex-wrap items-center justify-center gap-4 pt-[10vh] text-white">
+      <Card className="mx-4 flex w-full max-w-[1000px] flex-col items-center justify-center gap-4 p-4 md:flex-row md:items-start">
+        
+      </Card>
+    </main>
+  );
 };
 
 export default CartPage;
