0fc1e3837ff8b59f43ccc665e8a3d5439040eeb1
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-06 21:01:22 +0800

    Add checkout page component

---
+++ app/u/checkout/page.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { ReceiptText } from "lucide-react";
+
+const CheckoutPage = () => {
+  return (
+    <main className="flex flex-col items-center justify-center gap-4 pt-[5vh] text-white">
+      <Card className="mx-4 flex w-full max-w-[300px] flex-col flex-wrap gap-4 p-4 ">
+        <section className="flex items-center justify-between">
+          <h1 className="flex gap-2 text-2xl font-bold">Checkout</h1>
+          <ReceiptText className="ml-2" />
+        </section>
+        <Separator />
+        <section className="flex flex-col gap-2">
+          <div className="flex flex-col">
+            <p className="text-sm font-semibold">Blue Collar x3</p>
+            <span className="text-xs font-medium">₱ 200.00</span>
+          </div>{" "}
+          <div className="flex flex-col">
+            <p className="text-sm font-semibold">Blue Collar x3</p>
+            <span className="text-xs font-medium">₱ 200.00</span>
+          </div>{" "}
+          <div className="flex flex-col">
+            <p className="text-sm font-semibold">Blue Collar x3</p>
+            <span className="text-xs font-medium">₱ 200.00</span>
+          </div>{" "}
+          <div className="flex flex-col">
+            <p className="text-sm font-semibold">Blue Collar x3</p>
+            <span className="text-xs font-medium">₱ 200.00</span>
+          </div>{" "}
+          <div className="flex flex-col">
+            <p className="text-sm font-semibold">Blue Collar x3</p>
+            <span className="text-xs font-medium">₱ 200.00</span>
+          </div>
+        </section>
+        <Separator />
+        <section className="flex flex-col">
+          <p className="text-sm font-semibold">Total Items: 14</p>
+          <p className="text-sm font-semibold">Checkout Price: 200,000.00</p>
+        </section>
+        {/*  check if there is address available in the user */}
+        <Button className="w-full grow">Place Order</Button>
+      </Card>
+    </main>
+  );
+};
+
+export default CheckoutPage;
