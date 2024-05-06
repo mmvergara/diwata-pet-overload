de4c231258d037d94594d9c909e704c5c7a96526
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-06 18:39:21 +0800

    Add CartProductCard component to CartPage

--- app/u/cart/page.tsx
+++ app/u/cart/page.tsx
@@ -1,10 +1,11 @@
+import { CartProductCard } from "@/components/CartProductCard";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { ShoppingBag } from "lucide-react";
 
 const CartPage = () => {
   return (
-    <main className="flex flex-wrap items-center justify-center gap-4 pt-[10vh] text-white">
+    <main className="flex flex-col items-center justify-center gap-4 pt-[5vh] text-white">
       <Card className="mx-4 flex w-full max-w-[500px]  flex-row flex-wrap items-center justify-around gap-4 p-4 ">
         <div className="grow rounded-md bg-slate-100 p-2 font-semibold">
           Total Items: 14
@@ -17,6 +18,16 @@ const CartPage = () => {
           <ShoppingBag size={16} className="ml-2" />
         </Button>
       </Card>
+      <section className="flex flex-wrap justify-center gap-4">
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+        <CartProductCard />
+      </section>
     </main>
   );
 };
