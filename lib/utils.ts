51de02ab19e751ba524a9b7a297fef0e004d89b4
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-07 11:02:55 +0800

    Add dbCategories and dbCategoryToFrontend constants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { CATEGORY } from "@prisma/client";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const dbCategories = Object.values(CATEGORY);
+export const dbCategoryToFrontend = {
+  COLLARSANDLEASHES: "Collars & Leashes",
+  TOYS: "Toys",
+  APPAREL: "Apparel",
+  BEDDING: "Bedding",
+  FOODANDBOWLS: "Food & Bowls",
+  TRAVELACCESSORIES: "Travel Accessories",
+  GROOMING: "Grooming",
+  HEALTHANDWELLNESS: "Health & Wellness",
+  TRAININGAIDS: "Training Aids",
+  IDTAGS: "ID Tags",
+};
