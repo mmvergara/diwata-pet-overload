c7464e55a5eff91eec76f9088b20afb04a99649b
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-08 06:39:55 +0800

    Add PayPal payment functionality to PaymentDialog component

--- components/PaymentDialog.tsx
+++ components/PaymentDialog.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,10 +9,79 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { NEXT_PUBLIC_PAYPAL_CLIENT_ID } from "@/config-public";
+import { UserCartProducts } from "@/db/cart";
+import {
+  CreateOrderActions,
+  CreateOrderData,
+  OnApproveActions,
+  OnApproveData,
+} from "@paypal/paypal-js";
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+type Props = {
+  UserCartProducts: UserCartProducts;
+};
+export function PaymentDialog({ UserCartProducts }: Props) {
+  if (UserCartProducts === null || UserCartProducts === undefined) return <></>;
 
-export function PaymentDialog() {
+  const items = UserCartProducts.map((product) => {
+    return {
+      name: product.product.name,
+      quantity: product.quantity.toString(),
+      unit_amount: {
+        currency_code: "PHP",
+        value: product.product.price.toString(),
+      },
+    };
+  });
+
+  const amountValue = UserCartProducts.reduce(
+    (acc, item) => acc + item.product.price * item.quantity,
+    0,
+  ).toString();
+
+  const onCreateOrder = (
+    data: CreateOrderData,
+    actions: CreateOrderActions,
+  ) => {
+    return actions.order.create({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          items,
+          amount: {
+            value: amountValue,
+            currency_code: "PHP",
+            breakdown: {
+              item_total: {
+                currency_code: "PHP",
+                value: amountValue,
+              },
+            },
+          },
+        },
+      ],
+    });
+  };
+
+  const onApproveOrder = async (
+    data: OnApproveData,
+    actions: OnApproveActions,
+  ): Promise<void> => {
+    console.log(data);
+    console.log(data);
+    console.log(actions);
+    try {
+      return actions?.order?.capture().then((details) => {
+        console.log(details);
+        console.log(details);
+        console.log(details);
+      });
+    } catch (error) {
+      return;
+    }
+  };
+  console.log(NEXT_PUBLIC_PAYPAL_CLIENT_ID);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -19,29 +89,40 @@ export function PaymentDialog() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Pay Using Paypal</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            {`You will be redirected to Paypal to complete your payment.`}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
+        <div className="grid gap-4 py-4"></div>
         <DialogFooter>
+          <PayPalScriptProvider
+            options={{
+              clientId: NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+              currency: "PHP",
+              intent: "capture",
+            }}
+          >
+            <PayPalButtons
+              createOrder={onCreateOrder}
+              onApprove={onApproveOrder}
+              style={{ layout: "horizontal" }}
+            />
+          </PayPalScriptProvider>
           <Button type="submit">Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 }
+
+[
+  {
+    field:
+      "/purchase_units/@reference_id=='default'/amount/breakdown/item_total",
+    location: "body",
+    issue: "ITEM_TOTAL_REQUIRED",
+    description:
+      "If item details are specified (items.unit_amount and items.quantity) corresponding amount.breakdown.item_total is required.",
+  },
+];
