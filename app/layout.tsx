26a2e79e768e1c838f31331ae4245bfc48749886
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-08 06:40:18 +0800

    Remove unnecessary import statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
