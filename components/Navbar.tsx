a7ea116c73f740f2e8fff62024557b81cc488c3f
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-07 10:26:39 +0800

    Refactor Navbar component to include authentication and user avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,49 @@
-"use client";
-import { ShoppingCart } from "lucide-react";
-import { Button } from "./ui/button";
-import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { auth } from "@/auth";
+import { Button } from "./ui/button";
+import { ShoppingCart } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-const Navbar: React.FC = () => {
-  const session = useSession();
+const Navbar: React.FC = async () => {
+  const session = await auth();
+  console.log({ session });
   return (
     <nav className="flex items-center justify-center bg-[#6b3614] px-4 py-2 text-white shadow-lg drop-shadow-md">
       <div className="flex h-[40px] w-full max-w-[1240px] items-center justify-between">
         <Link href="/" className="text-2xl font-bold">
           logo
         </Link>
-        {session.status !== "loading" && (
-          <div className="font-semibold">
-            {session.status === "unauthenticated" && (
-              <Link href="/auth/sign-in">
-                <Button
-                  variant="outline"
-                  className="ml-4 font-bold text-black shadow-md"
-                >
-                  Sign In
-                </Button>
-              </Link>
-            )}
-            <Link href="/u/cart">
+        <div className="font-semibold">
+          {!session?.user?.email ? (
+            <Link href="/auth/sign-in">
               <Button
                 variant="outline"
-                className="ml-4 h-[35px] w-[35px] rounded-full p-[8px] font-bold text-black shadow-md"
+                className="ml-4 font-bold text-black shadow-md"
               >
-                <ShoppingCart className="text-brownPri hover:text-brownSec" />
+                Sign In
               </Button>
             </Link>
-          </div>
-        )}
+          ) : (
+            <div className="flex items-center gap-2">
+              <Link href="/u/cart">
+                <Button
+                  variant="outline"
+                  className="ml-4 h-[35px] w-[35px] rounded-full p-[8px] font-bold text-black shadow-md"
+                >
+                  <ShoppingCart className="text-brownPri hover:text-brownSec" />
+                </Button>
+              </Link>
+              <Link href="/u/dashboard">
+                <Avatar className="max-h-[35px] max-w-[35px] ">
+                  <AvatarImage src={session.user.image} />
+                  <AvatarFallback className="text-md text-brownPri shadow-md hover:text-brownSec">
+                    U
+                  </AvatarFallback>
+                </Avatar>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
