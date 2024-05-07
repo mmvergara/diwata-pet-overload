02ccea754a4ebec213fe94468a96195837b85ca1
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-07 16:22:17 +0800

    Refactor AddToCartBtn component and integrate with cart functionality

--- components/AddToCartBtn.tsx
+++ components/AddToCartBtn.tsx
@@ -1,49 +1,43 @@
 "use client";
 
-import { useState, type ComponentProps } from "react";
-import { Button } from "./ui/button";
 import { CircleCheckIcon, ShoppingCartIcon } from "lucide-react";
+import { SubmitButton } from "./SubmitBtn";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { addProductToCart } from "@/db/cart";
+import { toast } from "react-toastify";
 
-type Props = ComponentProps<"button"> & {
+type Props = {
   productID: string;
 };
 
-export function AddToCardBtn({ children, productID, ...props }: Props) {
-  const [pending, setPending] = useState(false);
+export function AddToCardBtn({ productID }: Props) {
   const [added, setAdded] = useState(false);
 
   const handleAddToCart = async () => {
-    setPending(true);
-    setTimeout(() => {
-      setPending(false);
-      setAdded(true);
-    }, 2000);
+    const { error } = await addProductToCart(productID);
+    if (error) return toast.error(error);
+    setAdded(true);
   };
 
   return (
-    <Button
-      {...props}
-      onClick={handleAddToCart}
-      disabled={pending || added}
-      variant="secondary"
-      className={cn(
-        `w-full font-semibold`,
-        added
-          ? `bg-emerald-600 text-white hover:bg-emerald-500`
-          : `bg-[hsl(240,5%,92%)] text-black hover:bg-[hsl(240,5%,85%)]`,
-      )}
-    >
-      {added
-        ? "Added to Cart!"
-        : pending
-          ? "Adding To Cart . .  ."
-          : "Add to Cart"}
-      {added ? (
-        <CircleCheckIcon size={20} className="ml-2" />
-      ) : (
-        <ShoppingCartIcon size={16} className="ml-2" />
-      )}
-    </Button>
+    <form className="w-full" action={handleAddToCart}>
+      <SubmitButton
+        pendingText="Adding To Cart . .  ."
+        className={cn(
+          `w-full font-semibold`,
+          added
+            ? `bg-emerald-600 text-white hover:bg-emerald-500`
+            : `bg-[hsl(240,5%,92%)] text-black hover:bg-[hsl(240,5%,85%)]`,
+        )}
+      >
+        {added ? "Added to Cart" : "Add to Cart"}
+        {added ? (
+          <CircleCheckIcon size={20} className="ml-2" />
+        ) : (
+          <ShoppingCartIcon size={16} className="ml-2" />
+        )}
+      </SubmitButton>
+    </form>
   );
 }
