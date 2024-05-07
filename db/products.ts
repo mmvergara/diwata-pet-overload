ef23dd618965c29a5c245068d70ea3a94b52eed3
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-07 11:47:22 +0800

    Add createProduct function to handle product creation

---
+++ db/products.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { utapi } from "@/lib/uploadthing";
+import { productFormSchema } from "@/lib/zod";
+import { CATEGORY } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+export const createProduct = async (formData: FormData) => {
+  const name = formData.get("name") as string;
+  const description = formData.get("description") as string;
+  const price = Number(formData.get("price"));
+  const quantity = Number(formData.get("quantity"));
+  const category = formData.get("category") as CATEGORY;
+  const image = (formData.get("image") as File) || null;
+
+  const formValues = { name, description, price, quantity, category, image };
+  const { error } = await productFormSchema.safeParseAsync(formValues);
+  if (error) return { error: error.issues[0].message };
+
+  let createdProductId = null;
+  try {
+    // upload to uploadthing
+    const [{ data, error }] = await utapi.uploadFiles([image]);
+    if (error) return { data: null, error: "Error uploading image" };
+
+    if (!data) return { data: null, error: "Error uploading image" };
+
+    const res = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        stock: quantity,
+        category,
+        image: data.url,
+      },
+    });
+    createdProductId = res.id;
+  } catch (error) {
+    console.log(error);
+    return { error: "Error creating product" };
+  }
+  if (createdProductId) redirect(`/product/${createdProductId}`);
+  return { error: null };
+};
