3a21b59354a874f1c64be2ec1b53e0ae2b1c4b22
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-07 12:53:28 +0800

    Update sign out button and add logout functionality

--- app/u/dashboard/page.tsx
+++ app/u/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import { auth } from "@/auth";
+import { signOut } from "@/auth";
+import { SubmitButton } from "@/components/SubmitBtn";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
@@ -12,17 +13,25 @@ import {
 import Link from "next/link";
 
 const DashboardPage = async () => {
+  const handleLogout = async () => {
+    "use server";
+    await signOut({ redirectTo: "/home" });
+  };
   return (
     <main className="px-4 pb-[20vh] pt-[5vh]">
       <div className="mx-auto flex w-full max-w-[780px] items-center justify-center gap-2 pb-2">
         <Button className="flex grow gap-2">
-          {" "}
           Continue Shopping <Store size={16} />
         </Button>
-        <Button className="order-red-500 flex max-w-[200px] grow gap-2 rounded-md border-2 border-red-500 bg-white font-medium text-red-500 hover:bg-btnWhitePri">
-          {" "}
-          Sign Out <LogOut />
-        </Button>
+        <form action={handleLogout}>
+          <SubmitButton
+            className="order-red-500 flex max-w-[200px] grow gap-2 rounded-md border-2 border-red-500 bg-white font-medium text-red-500 hover:bg-btnWhitePri"
+            type="submit"
+            pendingText="Logging out..."
+          >
+            Logout <LogOut />
+          </SubmitButton>
+        </form>
       </div>
       <section className="mx-auto flex w-full max-w-[780px] flex-wrap items-center justify-center gap-2 text-white">
         <Link href="/cart" className="grow">
