3d554dbe42a1063d47a644212a54aff6fc6f105e
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-09 16:17:00 +0800

    Add avatar upload functionality and update user full name

--- db/user.ts
+++ db/user.ts
@@ -1,6 +1,41 @@
 "use server";
 import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
+import { utapi } from "@/lib/uploadthing";
+import { updateFullnameSchema } from "@/lib/zod";
+import { revalidatePath } from "next/cache";
+
+export const updateAvatar = async (formData: FormData) => {
+  
+
+  const [{ data, error }] = await utapi.uploadFiles([image]);
+  
+}
+
+export const updateUserFullName = async (formData: FormData) => {
+  const session = await auth();
+  if (!session) return { error: null };
+  const fullName = formData.get("fullName");
+  const { error, data } = await updateFullnameSchema.safeParseAsync({
+    fullName,
+  });
+  if (error) return { error: error.issues[0].message };
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        fullName: data.fullName,
+      },
+    });
+  } catch (error) {
+    return { error: "An error occurred while updating your full name" };
+  }
+  revalidatePath("/u/profile");
+  return { error: null };
+};
 
 export const getCurrentUserData = async () => {
   const session = await auth();
@@ -21,17 +56,6 @@ export const getUserById = async (id: string) => {
   try {
     return await prisma.user.findUnique({
       where: { id },
-    });
-  } catch (error) {
-    return null;
-  }
-};
-
-export const updateUserFullName = async (id: string, fullName: string) => {
-  try {
-    return await prisma.user.update({
-      where: { id },
-      data: {  },
     });
   } catch (error) {
     return null;
