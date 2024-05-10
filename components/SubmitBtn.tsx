3a51fe4083023b360d18944718f9d78d9776c6fc
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-10 11:53:10 +0800

    Update SubmitButton component to support rendering pendingText as React.ReactNode

--- components/SubmitBtn.tsx
+++ components/SubmitBtn.tsx
@@ -5,14 +5,14 @@ import { type ComponentProps } from "react";
 import { Button } from "./ui/button";
 
 type Props = ComponentProps<"button"> & {
-  pendingText: string;
+  pendingText: string | React.ReactNode;
 };
 
 export function SubmitButton({ children, pendingText, ...props }: Props) {
   const { pending } = useFormStatus();
 
   return (
-    <Button {...props} type="submit" aria-disabled={pending}>
+    <Button {...props} type="submit" aria-disabled={pending} disabled={pending}>
       {pending ? pendingText : children}
     </Button>
   );
