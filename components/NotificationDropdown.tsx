ac91ce8f74039262ba286b920da0a7d31240535c
mmvergara <mark.jesusmanabat@gmail.com>
2024-05-10 09:32:52 +0800

    Refactor notification icons and links in NotificationDropdown component

--- components/NotificationDropdown.tsx
+++ components/NotificationDropdown.tsx
@@ -31,17 +31,21 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { getCurrentUser10RecentNotifications } from "@/db/notification";
+import { auth } from "@/auth";
+import Link from "next/link";
 
 const notificationIcon = {
-  NEW_ORDER: <Receipt />,
-  NEW_REVIEW: <Star />,
-  ORDER_INTRANSIT: <Truck />,
-  ORDER_DELIVERED: <PackageCheck />,
+  NEW_ORDER: <ReceiptText className="mr-2 mt-1 h-5 w-6 text-cyan-800" />,
+  NEW_REVIEW: <Star className="mr-2 mt-1 h-5 w-6 text-amber-400" />,
+  ORDER_INTRANSIT: <Truck className="mr-2 mt-1 h-5 w-6 text-fuchsia-800" />,
+  ORDER_DELIVERED: (
+    <PackageCheck className="mr-2 mt-1 h-5 w-6 text-emerald-800" />
+  ),
 };
 
 export async function NotificationDropdown() {
   const notifications = (await getCurrentUser10RecentNotifications()) || [];
-  console.log(notifications);
+  console.log({ notifications });
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -54,7 +58,79 @@ export async function NotificationDropdown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-[300px]">
         <DropdownMenuGroup>
-          <DropdownMenuItem className="flex items-start justify-start bg-emerald-200">
+          {notifications.map((notif) => {
+            switch (notif.type) {
+              case "NEW_ORDER":
+                return (
+                  <Link href={`/u/orders/${notif.contentId}`}>
+                    <DropdownMenuItem className="flex cursor-pointer items-start justify-start bg-cyan-200">
+                      <ReceiptText className="mr-2 mt-1 h-4 w-4 text-cyan-800" />
+                      <div className="flex flex-col">
+                        <span className="font-semibold text-black">
+                          New Order
+                        </span>
+                        <span className="text-black-800 text-xs font-medium">
+                          {notif.content}
+                        </span>
+                      </div>
+                    </DropdownMenuItem>
+                  </Link>
+                );
+
+              case "NEW_REVIEW":
+                return (
+                  <Link href={`/product/${notif.contentId}`}>
+                    <DropdownMenuItem className="flex cursor-pointer items-start justify-start bg-amber-200">
+                      <Star className="text-black-800 mr-2 mt-1 h-4 w-4" />
+                      <div className="flex flex-col">
+                        <span className="font-semibold text-black">
+                          New Review
+                        </span>
+                        <span className="text-black-800 text-xs font-medium">
+                          {notif.content}
+                        </span>
+                      </div>
+                    </DropdownMenuItem>
+                  </Link>
+                );
+
+              case "ORDER_INTRANSIT":
+                return (
+                  <Link href={`/u/orders/${notif.contentId}`}>
+                    <DropdownMenuItem className="flex cursor-pointer items-start justify-start bg-fuchsia-200">
+                      <Truck className="mr-2 mt-1 h-4 w-4 text-fuchsia-800" />
+                      <div className="flex flex-col">
+                        <span className="text-black-900 font-semibold">
+                          Order is in Transit
+                        </span>
+                        <span className="text-black-800 text-xs font-medium">
+                          {notif.content}
+                        </span>
+                      </div>
+                    </DropdownMenuItem>
+                  </Link>
+                );
+
+              case "ORDER_DELIVERED":
+                return (
+                  <Link href={`/u/orders/${notif.contentId}`}>
+                    <DropdownMenuItem className="flex cursor-pointer items-start justify-start bg-emerald-200">
+                      <PackageCheck className="mr-2 mt-1 h-4 w-4 text-emerald-800" />
+                      <div className="flex flex-col">
+                        <span className="font-semibold text-black">
+                          Order Delivered
+                        </span>
+                        <span className="text-black-800 text-xs font-medium">
+                          {notif.content}
+                        </span>
+                      </div>
+                    </DropdownMenuItem>
+                  </Link>
+                );
+            }
+          })}
+
+          {/* <DropdownMenuItem className="flex items-start justify-start bg-emerald-200">
             <PackageCheck className="mr-2 mt-1 h-4 w-4 text-emerald-800" />
             <div className="flex flex-col">
               <span className="font-semibold text-black">Order #1234</span>
@@ -83,15 +159,6 @@ export async function NotificationDropdown() {
               </span>
             </div>
           </DropdownMenuItem>
-          <DropdownMenuItem className="flex items-start justify-start bg-cyan-200">
-            <ReceiptText className="mr-2 mt-1 h-4 w-4 text-cyan-800" />
-            <div className="flex flex-col">
-              <span className="font-semibold text-black">New Order</span>
-              <span className="text-black-800 text-sm font-medium">
-                A New Order has been placed.
-              </span>
-            </div>
-          </DropdownMenuItem>
 
           <DropdownMenuItem className="flex items-start justify-start bg-gray-200">
             <PackageCheck className="mr-2 mt-1 h-4 w-4 text-gray-800" />
@@ -106,7 +173,7 @@ export async function NotificationDropdown() {
                 A New Order has been placed.
               </span>
             </div>
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
